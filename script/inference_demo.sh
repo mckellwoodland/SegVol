cbb9e6d7e98e48df66cedd9973985f86bf6743df
mewoodland <mewoodland@mdanderson.org>
2024-01-26 14:11:35 -0600

    add clip ckpt

--- script/inference_demo.sh
+++ script/inference_demo.sh
@@ -1,8 +1,10 @@
-export segvol_ckpt="path/to/SegVol_v1.pth"
-export work_dir="./work_dir"
-export demo_config_path="./config/config_demo.json"
+export segvol_ckpt="SegVol/model/SegVol_v1.pth"
+export work_dir="SegVol/logs/demo/"
+export demo_config_path="SegVol/config/config_demo.json"
+export clip_ckpt="SegVol/model/CLIP"
 
-CUDA_VISIBLE_DEVICES=0 python inference_demo.py \
---resume $segvol_ckpt \
--work_dir $work_dir \
---demo_config $demo_config_path 
+CUDA_VISIBLE_DEVICES=0 python SegVol/inference_demo.py \
+				--resume $segvol_ckpt \
+				-work_dir $work_dir \
+				--demo_config $demo_config_path \
+				--clip_ckpt $clip_ckpt
