47f75931a4635ccd7fb95c69137db463eb781a32
mewoodland <mewoodland@mdanderson.org>
2024-01-26 15:00:36 -0600

    adding inference code

---
+++ script/inference.sh
@@ -0,0 +1,10 @@
+export segvol_ckpt="SegVol/model/SegVol_v1.pth"
+export work_dir="SegVol/logs/{DATASET}/"
+export demo_config_path="SegVol/config/config_{DATASET}.json"
+export clip_ckpt="SegVol/model/CLIP"
+
+python SegVol/inference.py \
+	--resume $segvol_ckpt \
+	--work_dir $work_dir \
+	--demo_config $demo_config_path \
+	--clip_ckpt $clip_ckpt
