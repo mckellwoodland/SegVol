8cd6711649fa2efe6d932ef36d0cf5f6cd34dca0
mewoodland <mewoodland@mdanderson.org>
2024-01-29 15:26:57 -0600

    make general

--- script/inference.sh
+++ script/inference.sh
@@ -1,10 +1,10 @@
 export segvol_ckpt="SegVol/model/SegVol_v1.pth"
-export work_dir="SegVol/logs/bladder/"
-export demo_config_path="SegVol/config/config_bladder.json"
+export work_dir="SegVol/logs/{LOG NAME}/"
+export config_path="SegVol/config/{JSON file}"
 export clip_ckpt="SegVol/model/CLIP"
 
 python SegVol/inference.py \
 	--resume $segvol_ckpt \
 	--work_dir $work_dir \
-	--demo_config $demo_config_path \
+	--config $config_path \
 	--clip_ckpt $clip_ckpt
